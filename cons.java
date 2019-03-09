34ae420f0ee73e8a63a6ecd793e043913d0e3a0a
vamshikusa <40212124+vamshikusa@users.noreply.github.com>
2019-03-09 16:00:59 +0530

    Add files via upload

---
+++ cons.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class cons
+{
+	cons()
+	{System.out.println("0 args");
+	}
+		
+	cons(int a)
+	{//int a;
+		System.out.println("\n"+"entered element is:"+a);
+	}	
+public static void main(String args[])
+{ 
+
+//int a=1,b=5;
+cons ob=new cons();
+cons ob1=new cons(10);
+// System.out.println("a="+a+"\t"+"b="+b+"\n"+"sum="+(a+b));
+
+}
+}
